c180b9406611dac4492505444c99fe4594ff62d5
fangstarting <101423922+fangstarting@users.noreply.github.com>
2024-05-23 22:22:20 +0800

    Create test.java

---
+++ src/test/java/com/fzipp/pay/algorithm/test.java
@@ -0,0 +1,109 @@
+tchoice: 
+-You like singing and dancing.
+2. The fairy story "Snow White”is very interesting.e	A. So do Ie	B.So I do	C.I do soe	D.Do I soe
+/A so ht ise	B. So is it 	\C. It is soer	D.Is it soe
+3. You didn't go for an outing at the seashore.c
+A. Neither do Ic
+B/Neither did
+/I didn't neither
+Did neither Iec
+-Look.(There c
+Oh, there
+A.comes the bus; comes it、e	B the bus comes; it comes
+C.comes the bus; it comes	D.the bus comes; comes it
+5. Not until) 	sixteen	to school.e
+A he was; did he gY	B he was; he went
+C. was he; he did go	D.was he: went he
+6. Hardly	when it began to rain.
+. had he got home	B.he had got home
+C.had got home he	D.he home had got
+No sooner 	than he fell asleep.
+A. his head had touched the pillowc	B. had his head touched the pillow
+C.touched the pillow his head had	D. touched the pillow had his head
+A. did Lthink of itc	B. I thought of it
+C.did think I of it	D. thought of it I
+9.(Seldom
+A. LíLi her feelings showed	B. did show LiLi her feelingse
+C. LiLi showed her feelings	D. did LiLi show here feelings
+10. Wever before in her life	such beautiful and precious jewelry.a
+B. did/he see
+he sawe
+D she has seen
+C. has she seen
+Many a time	the chess competition.
+A had taken he part in	B. he taken part in had
+taken part in	D.he had taken part in
+So busy	that he has ho time to spare.
+the was	B.was he	6.he ise
+13.	A. A girl in/came	 wth a anch of lowers in ber  cane in a sr
+In came a	D. Did A girl come in l
+/14)	In the clear blue sky
+A. does shine the bright moon. 	B the bright moon does shinee
+C. shines the bright moon	D the bright moon shines
+15.1	(Onty in this way
+A we can well do it	B. can we well do it
+.we can do it well	D. can we do it well
+16. Only	(whenthe has finished his homework 
+A is able to he play with his friends for a while
+B he able to play with is his friends for a while
+C.is he able to play with his friends for a while
+Qhe is able to play with his friends for a while
+,he continued his study.
+.A.Late as it was	B.As it was late
+C.Late although it wasl
+、D Although was it late
+when be left.
+B.spoke he	C. did he speak	D.he spce
+-I don'tthink I(can walk any further.e
+Let's stop here for a rest.
+A. Neither am Ic
+C.I think so	B. Neither can I
+20. John's not been to London.e	D. I don't think so
+A. Ben isn't either
+C. Nor Ben has	B. Neither is Ben
+21. The doctor asked Charlie to breathe deeply.s	D. Neither has Ben
+A Charlie does so
+C. So does Charlie	B. Charlie did so
+22	so busy,1 should go with you.	D.So did Charlie
+A. Were I not
+B. Was I not
+
+were noter
+A. is the deske
+'s, it is a desk for the teacher	o.nas the desk of the teacher'
+ds On the top of the hill	D. has it a desk of the teachere
+A does a temple stand
+B. a temple stands theres
+C.stands a temple	D. a temple stands
+Not only	but also she likes singing.r
+A she likes painting	B.does she like paintinge
+C.like painting she	D. she does like paintinge
+/26) Now
+come your turn	B. does your turn comee
+your turn comes	D. your tur does comee
+happv.
+A You may be	B. May you bee
+C. Be you mays	D. May be you
+28. Little	how happy I(had been.
+y she had realized	B.did she realize
+ she realized	-	D. has she realized
+considered the alternatives more carefully, they would have realized that the
+tn
+second was better than the first.
+A.If the committee members have	B. Had the committee members been
+C. Though the committee members	D.Had the committee membersec
+the woman's possessionsa	that could carry them in a single suitcase
+A. Were so few	B. So few were
+C.Few were so	. D. There were so fewo bn
+ TE	(Only after)Ihad known him for sometime )er	to appreciate his real worth.e
+A. do I begine	B. did I begin
+D.had I begun 
+in his green T-shirt.
+B. here comes David
+y does David comes herer
+D. here David comes
+	<comes here David	D.Down jumped the thief	B. Did the thief jump downr
+C. Down the thief jumped
+to speak when the audience interrupted him.s	B.No sooner had he begune
+A Hardly had he begun	O D. Scarcely did he begin
+/Not until he began (
